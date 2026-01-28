504ab1f2f9038f7ea9e012db56ba2d92e1f6cad3
Michael Trullas <adoser2@hotmail.es>
2026-01-28 19:28:12 +0100

    s

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,37 +1,16 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CustomersTable } from './components/customers-table/customers-table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatButtonModule } from '@angular/material/button';
-import { ThemeService } from './services/theme.service';
-import { themeColors, themeModes } from './models/theme';
+import { ThemeSelector } from './components/theme-selector/theme-selector';
 
 @Component({
   selector: 'app-root',
   imports: [
     CustomersTable,
     MatToolbarModule,
-    MatSelectModule,
-    MatFormFieldModule,
-    CommonModule,
-    MatIconModule,
-    MatRadioModule,
-    MatMenuModule,
-    MatButtonModule],
+    ThemeSelector],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App implements OnInit {
-  themeService = inject(ThemeService);
-  themeColors = themeColors;
-  themeModes = themeModes;
-
-  ngOnInit() {
-    this.themeService.loadTheme();
-  }
+export class App {
 }
---
+++ src/app/components/theme-selector/theme-selector.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { MatSelectModule } from '@angular/material/select';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { ThemeService } from '../../services/theme.service';
+import { themeColors, themeModes } from '../../models/theme';
+
+@Component({
+  selector: 'app-theme-selector',
+  imports: [
+    MatSelectModule,
+    CommonModule,
+    MatIconModule,
+    MatRadioModule,
+    MatMenuModule,
+    MatButtonModule],
+  templateUrl: './theme-selector.html',
+  styleUrl: './theme-selector.scss'
+})
+export class ThemeSelector implements OnInit {
+  themeService = inject(ThemeService);
+  themeColors = themeColors;
+  themeModes = themeModes;
+
+  ngOnInit() {
+    this.themeService.loadTheme();
+  }
+}
